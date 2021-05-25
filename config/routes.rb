15e93a074207b4547e1241a0f58a0544c4c9e524
Marcus Heng <marcushwz@gmail.com>
2021-05-25 22:36:50 +0800

    Add root route for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
+  get "static_pages/home"
+  get "static_pages/help"
+
+  root to: "static_pages#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
