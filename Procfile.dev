5d4efb2d4c514c00eb2022681ca0c69b15298ca3
Marcus Heng <marcushwz@gmail.com>
2021-05-25 22:13:10 +0800

    Initial commit

---
+++ Procfile.dev
@@ -0,0 +1,3 @@
+server: bin/rails s
+assets: bin/webpack-dev-server
+reload: bundle exec guard
---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./app/**/*.html.erb", "./app/helpers/**/*.rb", "./app/javascript/**/*.js"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ["Rubik", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        nord: {
+          0: "#2e3440",
+          1: "#3b4252",
+          2: "#434C5E",
+          3: "#4c566a",
+          4: "#d8dee9",
+          5: "#e5e9f0",
+          6: "#eceff4",
+          7: "#8fbcbb",
+          8: "#88c0d0",
+          9: "#81a1c1",
+          10: "#5e81ac",
+          11: "#bf616a",
+          12: "#d08770",
+          13: "#ebcb8b",
+          14: "#a3be8c",
+          15: "#b48ead",
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
